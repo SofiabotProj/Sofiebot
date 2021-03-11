27b8370c15895afa3f0f508148814b73bd90c2f7
Sofiebot <creatorscontatoy@gmail.com>
2021-03-11 15:40:00 -0300

    First commit

---
+++ src/donasi.js
@@ -0,0 +1,21 @@
+const donasi = () => {
+	return `
+
+┏━━━━━━━━━━━━━━━━━━━━
+┃ *DONATE PARA COMPRAR PÃO*
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ *DONATE PARA CONPRAR PÃO* ❉⊰━━✿
+┃  
+┣━⊱ *PAO*
+┣⊱ 0895-7100-73737
+┣━⊱ *BANANA*
+┣⊱ 0823-3429-7175
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃  *SOFIA BOT =)*
+┗━━━━━━━━━━━━━━━━━━━━
+
+`
+}
+
+exports.donasi = donasi
